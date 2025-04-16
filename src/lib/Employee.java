79266bc9f5e1d2d03fc4c00d4c6c6fa0c7e4d00a
Matthew Theodore Lempas <99644704+viviole@users.noreply.github.com>
2025-04-17 04:08:40 +0700

    Mengubah if menjadi switch cases

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -53,22 +53,15 @@ public class Employee {
 	 */
 	
 	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+		int baseSalary;
+		switch (grade) {
+			case 1: baseSalary = 3000000; break;
+			case 2: baseSalary = 5000000; break;
+			case 3: baseSalary = 7000000; break;
+			default:
+				throw new IllegalArgumentException("Invalid grade: " + grade);
 		}
+    	monthlySalary = isForeigner ? (int)(baseSalary * 1.5) : baseSalary;
 	}
 	
 	public void setAnnualDeductible(int deductible) {	
