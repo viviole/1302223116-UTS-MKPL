a192adb6f10aba47052b7eabfef1c71fa4bc9aff
Matthew Theodore Lempas <99644704+viviole@users.noreply.github.com>
2025-04-17 07:10:30 +0700

    Menghapus dead code

--- src/lib/PersonalInfo.java
+++ src/lib/PersonalInfo.java
@@ -2,22 +2,13 @@ package lib;
 
 public class PersonalInfo {
     private String employeeId;
-    private String firstName;
-    private String lastName;
     private String idNumber;
-    private String address;
     private boolean isForeigner;
-    private Employee.Gender gender;
 
-    public PersonalInfo(String employeeId, String firstName, String lastName, String idNumber,
-                        String address, boolean isForeigner, boolean isMale) {
+    public PersonalInfo(String employeeId, String idNumber, boolean isForeigner) {
         this.employeeId = employeeId;
-        this.firstName = firstName;
-        this.lastName = lastName;
         this.idNumber = idNumber;
-        this.address = address;
         this.isForeigner = isForeigner;
-        this.gender = isMale ? Employee.Gender.MALE : Employee.Gender.FEMALE;
     }
 
     public boolean isForeigner() {
